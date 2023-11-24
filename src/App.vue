111dd1c6ef00a56c1f888067d12b2f7e7e9e1865
Arinionium <arinioniumheron@gmail.com>
2023-11-25 01:38:36 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <WeatherPage/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WeatherPage from './components/WeatherPage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WeatherPage
   }
 }
 </script>
