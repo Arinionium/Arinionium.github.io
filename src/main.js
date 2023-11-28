f9eb4f8532aadc7b4d0534bcd19366c0e6b492c3
Arinionium <arinioniumheron@gmail.com>
2023-11-28 06:05:44 +0200

    -Added chartJs and date adapter for it;

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router';
 import './assets/css/styles.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import 'chartjs-adapter-date-fns';
 
 Vue.config.productionTip = false
 
